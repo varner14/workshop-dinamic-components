d4aa581085ae4a8cb91c46aa4b8c7e9ca52f0293
vasilveira <varner.cardoso@engdb.com.br>
2025-02-19 21:03:01 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, ComponentRef, inject, TemplateRef, Type, viewChild, ViewContainerRef } from "@angular/core";
+import { WidgetComponent } from "./widget/widget.component";
+import { NgComponentOutlet } from "@angular/common";
+import { WeatherContentComponent } from "./widget/weather-content.component";
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <h1 class="page-title">ngContentOutlet Demo</h1>
+    <ng-template #contentTpl>
+      <app-weather-content />
+    </ng-template>
+    <main id="content">
+      <ng-container
+        [ngComponentOutlet]="component"
+        [ngComponentOutletInputs]="componentInputs"
+        [ngComponentOutletContent]="content"></ng-container>
+      <section class="toolbar">
+        <button (click)="createComponent()" class="create">Create Component</button>
+        <button (click)="destroyComponent()" class="destroy">Destroy Component</button>
+      </section>
+    </main>
+  `,
+  imports: [NgComponentOutlet, WeatherContentComponent]
 })
 export class AppComponent {
-  title = 'componentOutlet';
+  vcr = inject(ViewContainerRef);
+  contentTpl = viewChild<TemplateRef<unknown>>('contentTpl')
+  protected content: Node[][] = [];
+  protected component: Type<WidgetComponent> | null = null;
+  protected componentInputs = {
+    title: 'Weather',
+    description: 'Currently in Vienna:'
+  }
+   
+  createComponent() {
+    this.content = [
+      this.vcr.createEmbeddedView(this.contentTpl()!).rootNodes
+    ]
+    this.component = WidgetComponent;
+  }
+  destroyComponent() {
+    this.component = null;
+  }
 }
